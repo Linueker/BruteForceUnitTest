c6a5a0b52dbb8c13ca7b585ddd6adc9eb1736aa9
Linus Ekermo <linus.ekermo@hotmail.com>
2024-08-21 09:37:30 +0200

    Initial commit and solution

--- BruteForceExample/Program.cs
+++ BruteForceExample/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BruteForceExample
+﻿
+
+namespace BruteForceExample
 {
     internal class Program
     {
@@ -17,6 +19,18 @@
 
             while (!current.Equals(password))
             {
+                while (count == 3)
+                {
+                    Random rnd = new Random();
+                    int captcha = rnd.Next(1000, 9999);
+                    Console.WriteLine($"Enter the following number and press enter: {captcha}");
+                    int input = int.Parse(Console.ReadLine());
+                    if (input == captcha)
+                    {
+                        count = 0;
+                        break;
+                    }
+                }
                 for (int i = 0; i < pos.Length; i++)
                 {
                     if (pos[i] == alphabet.Length)
@@ -26,6 +40,8 @@
                     }
                 }
 
+                
+
                 current = (alphabet[pos[5]] + alphabet[pos[4]] + alphabet[pos[3]] + alphabet[pos[2]] + alphabet[pos[1]] + alphabet[pos[0]]).ToString();
 
                 if (count %100 == 0) Console.WriteLine(current);
@@ -34,9 +50,6 @@
             }
 
             Console.WriteLine($"Hittat password: {current}");
-
-
-
 
         }
     }
